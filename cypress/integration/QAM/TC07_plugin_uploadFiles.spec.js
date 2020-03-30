dfd4c893cda0818ce4965d04d2688b2c852b9ac9
bushralam <bushralam606@gmail.com>
2020-03-30 15:19:23 +0530

    cypress-file-upload v4.0.4

--- cypress/integration/QAM/TC07_plugin_uploadFiles.spec.js
+++ cypress/integration/QAM/TC07_plugin_uploadFiles.spec.js
@@ -4,9 +4,15 @@ describe('Working with plugins', function(){
         
         // upload file
         const fileName = 'SampleFile.pdf';
+        
+        /* For cypress-file-upload version < 4.0.0
         cy.fixture(fileName).then(fileContent => {
             cy.get('[type="file"]').upload({ fileContent, fileName, mimeType: 'application/pdf' });
-        })
+        })*/
+        
+        // For cypress-file-upload version > 4.0.0
+        cy.get('[type="file"]').attachFile(fileName);
+
         cy.get('[type="submit"]').click()
 
         cy.contains("You've uploaded a file.  Your notes on the file were:")
